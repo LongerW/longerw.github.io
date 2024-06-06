de7e27a4c37336559e6e3b7457e8a7da9ef87cbb
yilongwang <yilong.wang@igg.com>
2024-06-06 15:01:01 +0800

    Site updated: 2024-06-06 15:01:01

--- js/script.js
+++ js/script.js
@@ -140,8 +140,9 @@
     const span = $('<span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>')
     const script = $('<script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>')
     console.log('span',span);
+    const footer = $('footer')
     document.body.appendChild(script[0])
-    document.body.appendChild(span[0])
+    footer.append(span[0])
   })()
 
   
